db20f15d3ce6e19717e3ea38aed0061512efa409
[ajv] <venkateshsridhar22@gmail.com>
2022-01-28 14:24:15 +0530

    init

---
+++ theatre.js
@@ -0,0 +1,62 @@
+var theatre = [
+     {
+        'movie':'A',
+        'price': 100
+    },
+    {
+        'movie':'B',
+        'price': 120
+    },
+    {
+        'movie':'C',
+        'price': 130
+    },
+    {
+        'movie':'D',
+        'price': 140
+    },
+    {
+        'movie':'E',
+        'price': 150
+    },
+]
+
+//loop
+
+for(i=0; i<theatre.length; i++){
+    console.log(theatre[i]);
+}
+
+//while loop
+
+var i=0;
+while(i<theatre.length){
+    console.log(theatre[i]);
+    i++;
+}
+
+//do while loop
+
+var i=3;
+do{
+    console.log(theatre[i]);
+    i++;
+}while(i<theatre)
+
+//for each loop
+
+var i=1;
+for(;;){
+    if(i>3)break;
+    console.log(theatre);
+    i++;
+}
+
+//map
+
+let screen = theatre.map(function(val){
+    return val + "action movies";
+});
+console.log(screen);
+
+//
